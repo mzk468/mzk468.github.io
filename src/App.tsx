140cc3db281d780f8e2d4bd084cf0c50e397cad2
Zakariya <mzkhaled468@gmail.com>
2024-10-22 20:11:28 +0100

    Created clean slate

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,3 @@
-import { Me } from "./components/Me";
-//import { Projects } from "./components/Projects"; // Import the missing Projects component
-
-export default function App() {
-  return (
-    <div>
-      <Me />
-      {/*<Projects />*/}
-    </div>
-  );
+export function App() {
+    return <></>
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./main.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { App } from './App.tsx'
 
-// Bootstrap
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+)
